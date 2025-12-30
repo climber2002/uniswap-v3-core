d224dc7c5cbb471f9c02fa2903ac2e8ca1325641
Kai Wong <climber2002@gmail.com>
2025-12-30 21:12:31 +1100

    Create slot0

--- mine/src/UniswapV3Pool.sol
+++ mine/src/UniswapV3Pool.sol
@@ -10,6 +10,40 @@ contract UniswapV3Pool {
   uint24 public immutable fee;
   uint128 public immutable maxLiquidityPerTick;
 
+  struct Slot0 {
+    // Current price
+    uint160 sqrtPriceX96;
+    // Current tick
+    int24 tick;
+    // TODO: not used for now
+    // the most-recently updated index of the observations array
+    uint16 observationIndex;
+    // TODO: not used for now
+    // the current maximum number of observations that are being stored
+    uint16 observationCardinality;
+    // TODO: not used for now
+    // the next maximum number of observations to store, triggered in observations.write
+    uint16 observationCardinalityNext;
+    // TODO: not used for now
+    // the current protocol fee as a percentage of the swap fee taken on withdrawal
+    // represented as an integer denominator (1/x)%
+    uint8 feeProtocol;
+    // Whether the pool is locked
+    bool unlocked;
+  }
+
+  Slot0 public slot0;
+
+  /// @dev Mutually exclusive reentrancy protection into the pool to/from a method. This method also prevents entrance
+  /// to a function before the pool is initialized. The reentrancy guard is required throughout the contract because
+  /// we use balance checks to determine the payment status of interactions such as mint, swap and flash.
+  modifier lock() {
+      require(slot0.unlocked, 'LOK');
+      slot0.unlocked = false;
+      _;
+      slot0.unlocked = true;
+  }
+
 
   constructor(
     address _token0,
